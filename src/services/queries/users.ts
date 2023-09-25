1d3cf32afc1d3fb152bb4ce92c89e4b1dcaec041
AnmZ <anmit.ray911@gmail.com>
2023-09-25 20:36:39 +0530

    REFACTOR : refactored create user to check for uniqueness using redis sets

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,4 +1,4 @@
-import { usersKey } from '$services/keys';
+import { usernameUniquekeys, usersKey } from '$services/keys';
 import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
@@ -14,7 +14,13 @@ export const getUserById = async (id: string) => {
 
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
+    //check fr uniqueness
+    const exists = await client.sIsMember(usernameUniquekeys(),attrs.username);
+    if(exists){
+        throw new Error("UserName is taken")
+    }
     await client.hSet(usersKey(id),serialize(attrs));
+    await client.sAdd(usernameUniquekeys(),attrs.username);
     return id;
 };
 
